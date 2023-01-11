b28f287475833bbc7c8351238af6e201732fd4d7
alex6970 <alexandragoncalves6970@gmail.com>
2023-01-11 16:53:27 +0100

    Class init fixed

--- main.py
+++ main.py
@@ -13,36 +13,52 @@ import random
 # print(type(english_dict))
 # print(english_dict[2])
 
-# tkinter
-# tkinter in OOP ?
+# App GUI
+
 class wind1:
-    def __init__(self, master):
-        self.master = master
+    def __init__(self, root):
+        self.root = root
 
-        windowWidth = 500
-        windowHeight = 200
-        positionRight = int(master.winfo_screenwidth()/2 - windowWidth/2)
-        positionDown = int(master.winfo_screenheight()/2.5 - windowHeight/2)
-        master.geometry("{}x{}+{}+{}".format(windowWidth, windowHeight, positionRight, positionDown))
-        master.resizable(width=False, height=False)
+        self.win_width = 600
+        self.win_height = 400
+        # self.frame = ct.CTkFrame(root)
 
-        self.label = ct.CTkLabel(master, text="This is our first GUI!")
+        root.geometry("{}x{}+{}+{}".format(self.win_width, self.win_height, 500, 150))
+        root.resizable(width=False, height=False)
+
+        self.display_widgets()
+
+    def display_widgets(self):
+        self.label = ct.CTkLabel(self.root, text="This is our first GUI!")
         self.label.pack()
 
-        self.close_button = ct.CTkButton(master, text="Close", command=master.quit)
+        self.close_button = ct.CTkButton(self.root, text="Close", command=self.root.quit)
         self.close_button.pack()
 
+        
 
 def main():
-    app = ct.CTk()
-    gui = wind1(app)
-    app.mainloop() 
+    root = ct.CTk()
+    app = wind1(root)
+    root.mainloop() 
 
 # Runs only if compiled as own script, not as module
 if __name__ == '__main__':
     main()
 
 
+
+
+# class Demo1:
+#     def __init__(self, master):
+#         self.master = master
+#         self.frame = tk.Frame(self.master)
+#         self.button1 = tk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window)
+#         self.button1.pack()
+#         self.frame.pack()
+#     def new_window(self):
+#         self.newWindow = tk.Toplevel(self.master)
+#         self.app = Demo2(self.newWindow)
 
 # TODO :
 # Center frame and go on with the rest of it (geters and seter to create each frame ?)
