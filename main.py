ffbc3bc67ef803ade4afa74d01878e20206c8fec
alex6970 <alexandragoncalves6970@gmail.com>
2023-01-24 17:06:39 +0100

    Comment

--- main.py
+++ main.py
@@ -67,18 +67,7 @@ if __name__ == '__main__':
 
 
 
-# class Demo1:
-#     def __init__(self, master):
-#         self.master = master
-#         self.frame = tk.Frame(self.master)
-#         self.button1 = tk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window)
-#         self.button1.pack()
-#         self.frame.pack()
-#     def new_window(self):
-#         self.newWindow = tk.Toplevel(self.master)
-#         self.app = Demo2(self.newWindow)
-
 # TODO :
-# Continue GUI
+# Continue GUI => 3 Frames + refaire page 1 widgets espacés
 
 
